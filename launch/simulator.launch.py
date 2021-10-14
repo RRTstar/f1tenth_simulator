790214399da4ffc5fe7bd686d096572595ca3c3b
Joon <dsazz801@gmail.com>
2021-10-15 00:00:15 +0900

    update launch file

--- launch/simulator.launch.py
+++ launch/simulator.launch.py
@@ -4,18 +4,28 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+#from launch_ros.parameters_type import Parameters
 
 def generate_launch_description():
     # use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    param_dir = LaunchConfiguration(
+        'param_dir',
+         default= os.path.join(
+                get_package_share_directory('f1tenth_simulator'),
+                'param',
+                'params.yaml'))
+
     return LaunchDescription([
         DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
+            'param_dir',
+            default_value = param_dir,
+            description = 'param directory'),
         Node(
             package='joy',
             executable='joy_node',
-            name='f1tenth_simulator',
+            name='joy_node',
             # output='screen'
         ),
         Node(
@@ -25,6 +35,41 @@ def generate_launch_description():
             arguments=['$(find f1tenth_simulator)/maps/levine.yaml'],
             output='screen'
         ),
+        Node(
+            package = 'f1tenth_simulator',
+            executable = 'simulator',
+            name = 'simulator',
+            parameters = [param_dir],
+            output = 'screen'
+        ),
+        Node(
+            package= 'f1tenth_simulator',
+            executable= 'mux',
+            name = 'mux',
+            parameters = [param_dir],
+            output = 'screen'
+        ),
+        Node(
+            package= 'f1tenth_simulator',
+            executable= 'behavior_controller',
+            name = 'behavior_controller',
+            parameters = [param_dir],
+            output = 'screen'
+        ),
+        Node(
+            package= 'f1tenth_simulator',
+            executable= 'random_walk',
+            name = 'random_walk',
+            parameters = [param_dir],
+            output = 'screen'
+        ),
+        Node(
+            package= 'f1tenth_simulator',
+            executable= 'keyboard',
+            name = 'keyboard',
+            parameters = [param_dir],
+            output = 'screen'
+        )
         # Node(
         #     package='rviz2',
         #     executable='rviz2',
